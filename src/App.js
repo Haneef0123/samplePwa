45c7f4623a179728af829690beebc46364b00d70
Haneef0123 <gchandrasekaran@msystechnologies.com>
2021-10-23 18:30:13 +0530

    Sample rxdb

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import HeroList from './hero-list/hero-list';
+import HeroInsert from './hero-insert/hero-insert';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <button>Click Me</button>
+      {/* <button>Click Me</button> */}
+      <h1>RxDB Example - React</h1>
+            <HeroList/>
+            <HeroInsert/>
     </div>
   );
 }
